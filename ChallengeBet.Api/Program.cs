680c0ca810860cae016cbb81f0eb8ca3b4063207
Lucas Menger <lmenger@navcode.com.br>
2025-08-26 20:18:11 -0300

    feat(api): register AppDbContext with SQL Server provider

--- ChallengeBet.Api/Program.cs
+++ ChallengeBet.Api/Program.cs
@@ -1,25 +1,25 @@
+using ChallengeBet.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        sql => sql.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)
+    )
+);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
---
+++ ChallengeBet.Infrastructure/AppDbContext.cs
@@ -0,0 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ChallengeBet.Infrastructure;
+
+public class AppDbContext : DbContext
+{
+    
+}
